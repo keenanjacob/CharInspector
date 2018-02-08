ab6698151f2498075d4f735e9f4859d67eda6996
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-07 18:00:48 -0800

    An awesome program

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Keenan jacob
 #include<iostream>
 
 using namespace std;
@@ -6,19 +6,31 @@ using namespace std;
 int main()
 {
 
+char letter;
+
   cout<<"What character do you want to know about?\n";
 
+  cin>>letter;
+
+  if (letter>='A' && letter<='Z')
+  {
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<<letter<<" is an upper case letter!\n";
+  }
 
+  if (letter>='a' && letter<='z')
+  {
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<<letter<<" is a lower case letter!\n";
+  }
 
+  if (letter<'A'|| letter>'z')
+  {
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<letter<<"?! Pssh. What are you talking about?\n";
+  }
 
+  //no matter what they enter...
+  cout<<"The ASCII value is: "<<(int)letter<<endl;
   return 0;
 }
